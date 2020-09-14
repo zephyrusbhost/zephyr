ec3fccc5097d69f48d9e1b123e8276a5263805ee
sascha ludwig <>
2020-09-14 14:36:26 +0200

    removed typedef

--- subsys/usbh/Class/MSC/usbh_msc.h
+++ subsys/usbh/Class/MSC/usbh_msc.h
@@ -40,12 +40,6 @@
 *********************************************************************************************************
 */
 
-#ifdef USBH_MSC_MODULE
-#define USBH_MSC_EXT
-#else
-#define USBH_MSC_EXT extern
-#endif
-
 /*
 *********************************************************************************************************
 *                                               DEFINES
@@ -67,10 +61,9 @@
 *********************************************************************************************************
 */
 
-typedef uint8_t USBH_MSC_DATA_DIR;
 
 /* -------------------- MSC DEVICE -------------------- */
-typedef struct usbh_msc_dev
+struct usbh_msc_dev
 {
     struct usbh_ep BulkInEP;  /* Bulk IN  endpoint.                                   */
     struct usbh_ep BulkOutEP; /* Bulk OUT endpoint.                                   */
@@ -79,16 +72,16 @@ typedef struct usbh_msc_dev
     uint8_t State;  /* State of MSC device.                                 */
     uint8_t RefCnt; /* Cnt of app ref on this dev.                          */
     struct k_mutex HMutex;
-} USBH_MSC_DEV;
+};
 
-typedef struct msc_inquiry_info
+struct msc_inquiry_info
 {
     uint8_t DevType;
     uint8_t IsRemovable;
     uint8_t Vendor_ID[8];
     uint8_t Product_ID[16];
     uint32_t ProductRevisionLevel;
-} USBH_MSC_INQUIRY_INFO;
+};
 
 /*
 *********************************************************************************************************
@@ -96,7 +89,7 @@ typedef struct msc_inquiry_info
 *********************************************************************************************************
 */
 
-USBH_MSC_EXT const struct usbh_class_drv USBH_MSC_ClassDrv;
+extern const struct usbh_class_drv USBH_MSC_ClassDrv;
 
 /*
 *********************************************************************************************************
@@ -110,30 +103,30 @@ USBH_MSC_EXT const struct usbh_class_drv USBH_MSC_ClassDrv;
 *********************************************************************************************************
 */
 
-int usbh_msc_init(USBH_MSC_DEV *p_msc_dev,
+int usbh_msc_init(struct usbh_msc_dev *p_msc_dev,
                        uint8_t lun);
 
-uint8_t usbh_msc_max_lun_get(USBH_MSC_DEV *p_msc_dev,
+uint8_t usbh_msc_max_lun_get(struct usbh_msc_dev *p_msc_dev,
                                int *p_err);
 
-bool usbh_msc_unit_rdy_test(USBH_MSC_DEV *p_msc_dev,
+bool usbh_msc_unit_rdy_test(struct usbh_msc_dev *p_msc_dev,
                                  uint8_t lun,
                                  int *p_err);
 
-int usbh_msc_capacity_rd(USBH_MSC_DEV *p_msc_dev,
+int usbh_msc_capacity_rd(struct usbh_msc_dev *p_msc_dev,
                              uint8_t lun,
                              uint32_t *p_nbr_blks,
                              uint32_t *p_blk_size);
 
-int usbh_msc_std_inquiry(USBH_MSC_DEV *p_msc_dev,
-                             USBH_MSC_INQUIRY_INFO *p_msc_inquiry_info,
+int usbh_msc_std_inquiry(struct usbh_msc_dev *p_msc_dev,
+                             struct msc_inquiry_info *p_msc_inquiry_info,
                              uint8_t lun);
 
-int usbh_msc_ref_add(USBH_MSC_DEV *p_msc_dev);
+int usbh_msc_ref_add(struct usbh_msc_dev *p_msc_dev);
 
-int usbh_msc_ref_rel(USBH_MSC_DEV *p_msc_dev);
+int usbh_msc_ref_rel(struct usbh_msc_dev *p_msc_dev);
 
-uint32_t usbh_msc_read(USBH_MSC_DEV *p_msc_dev,
+uint32_t usbh_msc_read(struct usbh_msc_dev *p_msc_dev,
                        uint8_t lun,
                        uint32_t blk_addr,
                        uint16_t nbr_blks,
@@ -141,13 +134,13 @@ uint32_t usbh_msc_read(USBH_MSC_DEV *p_msc_dev,
                        void *p_arg,
                        int *p_err);
 
-uint32_t usbh_msc_write(USBH_MSC_DEV *p_msc_dev,
-                       uint8_t lun,
-                       uint32_t blk_addr,
-                       uint16_t nbr_blks,
-                       uint32_t blk_size,
-                       const void *p_arg,
-                       int *p_err);
+uint32_t usbh_msc_write(struct usbh_msc_dev *p_msc_dev,
+			uint8_t lun,
+			uint32_t blk_addr,
+			uint16_t nbr_blks,
+			uint32_t blk_size,
+			const void *p_arg,
+			int *p_err);
 
 /*
 *********************************************************************************************************
