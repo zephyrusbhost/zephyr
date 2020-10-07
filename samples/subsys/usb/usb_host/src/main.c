355eaab82aef192ac5b7161eef42a75e7fcf3fe6
sascha ludwig <>
2020-10-07 10:36:19 +0200

    changed sample

--- samples/subsys/usb/usb_host/src/main.c
+++ samples/subsys/usb/usb_host/src/main.c
@@ -4,42 +4,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-//#define DT_DRV_COMPAT atmel_sam0_usbh
-
 #include <zephyr.h>
 #include <sys/printk.h>
-#include <bsp_usbh_template.h>
 #include <usbh_msc.h>
 
 #include <logging/log.h>
 LOG_MODULE_REGISTER(main);
-
-// static void App_USBH_HID_ClassDevNotify(void *p_class_dev, CPU_INT08U state,
-// 					void *p_ctx)
-// {
-// 	USBH_HID_DEV *p_hid_dev;
-
-// 	(void)p_ctx;
-// 	p_hid_dev = (USBH_HID_DEV *)p_class_dev;
-
-// 	switch (state) {
-// 	case USBH_CLASS_DEV_STATE_CONN:
-// 		LOG_INF("HID device connected"); /* ---------------- HID DEVICE CONN'D ----------------- */
-// 		//APP_TRACE_INFO(("HID Demo: Device Connected\r\n"));
-// 		//  App_USBH_HID_DevConn(p_hid_dev);
-// 		break;
-
-// 	case USBH_CLASS_DEV_STATE_DISCONN:
-// 		LOG_INF("HID device disconnected"); /* ---------------- HID DEVICE CONN'D ----------------- */
-// 		/* ---------------- HID DEVICE REMOVED ---------------- */
-// 		//APP_TRACE_INFO(("HID Demo: Device Removed\r\n"));
-// 		//  App_USBH_HID_DevDisconn(p_hid_dev);
-// 		break;
-
-// 	default:
-// 		break;
-// 	}
-// }
 
 static void App_USBH_MSC_ClassNotify(void *p_class_dev, uint8_t is_conn,
 				     void *p_ctx)
@@ -81,14 +51,11 @@ static void App_USBH_MSC_ClassNotify(void *p_class_dev, uint8_t is_conn,
 void main(void)
 {
 
-	//printk("%d\n", DT_INST_PROP(0, buf_len));
 	int err = usbh_init();
 	printk("Init return %d\n", err);
 
 	uint8_t hc_nbr =
-		usbh_hc_add(&USBH_ATSAMX_HCD_DrvAPI,
-			    &USBH_ATSAMX_HCD_RH_API, &USBH_DrvBSP_Template,
-			    &err);
+		usbh_hc_add(&err);
 
 	err = usbh_reg_class_drv(&USBH_MSC_ClassDrv, App_USBH_MSC_ClassNotify,
 				 NULL);
@@ -98,7 +65,4 @@ void main(void)
 	if (err != 0) {
 		printk("error start hc %d\n", err);
 	}
-
-	LOG_INF("init all done");
-
 }
